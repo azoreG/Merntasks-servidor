d3fe349b8e6cc2ac6a5a8b095c16ec4797fac9ac
Jorge Eroza <george_erosa@hotmail.com>
2020-06-01 18:21:55 -0500

    cambio-menor

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json({extended: true}));
 
 //puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 //importar rutas
 app.use("/api/usuarios", require("./routes/usuarios"));
@@ -25,4 +25,4 @@ app.use("/api/proyectos", require("./routes/proyectos"));
 app.use("/api/tareas", require("./routes/tareas"));
 
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+app.listen(port,"0.0.0.0", () => console.log(`Server running on port: ${PORT}`))
