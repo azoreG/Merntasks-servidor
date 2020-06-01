7d9ae8bce28dcc5621cc93c0628c03c248d1a45b
Jorge Eroza <george_erosa@hotmail.com>
2020-06-01 18:11:40 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const conectarDB = require("./config/db");
+const cors = require("cors");
+
+//crear el servidor
+const app = express()
+
+//Conectar a la base de datos
+conectarDB();
+
+//Habilitar cors
+
+app.use(cors());
+
+//Habilitar express.json
+app.use(express.json({extended: true}));
+
+//puerto de la app
+const PORT = process.env.PORT || 4000;
+
+//importar rutas
+app.use("/api/usuarios", require("./routes/usuarios"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/proyectos", require("./routes/proyectos"));
+app.use("/api/tareas", require("./routes/tareas"));
+
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
